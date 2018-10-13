8262f0057e2e778031858df6d47410fd0109d939
Kamesuta <kamesuta@gmail.com>
2018-10-14 03:30:22 +0900

    initial commit

---
+++ src/main/java/net/minecraftforge/fml/relauncher/IFMLLoadingPlugin.java
@@ -0,0 +1,49 @@
+package net.minecraftforge.fml.relauncher;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.util.Map;
+
+public interface IFMLLoadingPlugin {
+	String[] getASMTransformerClass();
+
+	String getModContainerClass();
+
+	String getSetupClass();
+
+	void injectData(Map<String, Object> data);
+
+	String getAccessTransformerClass();
+
+	@Retention(RetentionPolicy.RUNTIME)
+	@Target(ElementType.TYPE)
+	public @interface TransformerExclusions {
+		public String[] value() default "";
+	}
+
+	@Retention(RetentionPolicy.RUNTIME)
+	@Target(ElementType.TYPE)
+	public @interface MCVersion {
+		public String value() default "";
+	}
+
+	@Retention(RetentionPolicy.RUNTIME)
+	@Target(ElementType.TYPE)
+	public @interface Name {
+		public String value() default "";
+	}
+
+	@Retention(RetentionPolicy.RUNTIME)
+	@Target(ElementType.TYPE)
+	public @interface DependsOn {
+		public String[] value() default {};
+	}
+
+	@Retention(RetentionPolicy.RUNTIME)
+	@Target(ElementType.TYPE)
+	public @interface SortingIndex {
+		public int value() default 0;
+	}
+}
