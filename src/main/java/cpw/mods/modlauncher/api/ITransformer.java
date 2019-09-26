805de62c63b42d420655721a104b05a5438c93f4
Kamesuta <kamesuta@gmail.com>
2019-09-26 20:52:34 +0800

    ITransformer

--- src/main/java/cpw/mods/modlauncher/api/ITransformer.java
+++ src/main/java/cpw/mods/modlauncher/api/ITransformer.java
@@ -1,4 +1,26 @@
 package cpw.mods.modlauncher.api;
 
+import java.util.Set;
+
 public interface ITransformer<T> {
+	String[] DEFAULT_LABEL = { "default" };
+
+	T transform(T input, ITransformerVotingContext context);
+
+	TransformerVoteResult castVote(ITransformerVotingContext context);
+
+	Set<Target> targets();
+
+	default String[] labels() {
+		return DEFAULT_LABEL;
+	}
+
+	enum TargetType {
+		CLASS,
+		METHOD,
+		FIELD
+	}
+
+	final class Target {
+	}
 }
---
+++ src/main/java/cpw/mods/modlauncher/api/TransformerVoteResult.java
@@ -0,0 +1,8 @@
+package cpw.mods.modlauncher.api;
+
+public enum TransformerVoteResult {
+	YES,
+	DEFER,
+	NO,
+	REJECT
+}
